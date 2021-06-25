d6edc43f21e07d28a9c0ea7393b7cb67155e6763
Aldwin Vlasblom <aldwin@avaq.it>
2021-06-25 17:42:20 +0200

    Update approach to testing
    Currently, we run the tests per preset, but since every module loads the
    eslint base, every preset will generate a warning when base rules are
    missing.
    
    The new approach combines all configuration in a single testing preset
    that contains all modules. Conflicts are not a problem, because we're
    only interested in whethe rules are defined anywhere, and a conflict
    will just lead to an override. I'll update #11 with the new test output.

---
+++ test/all.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var presets = [
+  'index',
+  'node',
+  'es6',
+  'jsx',
+  'module',
+  'typescript',
+];
+
+module.exports = {
+  extends: presets.map(function(preset){
+    return '../' + preset + '.js';
+  }),
+};
