32eea6c3a0032242d178bb93cad6c092699636f9
Danny Eisenga <d.eisenga@gmail.com>
2023-08-06 08:24:04 +0200

    Remove deprecated es6 rule

--- es6.js
+++ es6.js
@@ -29,7 +29,6 @@ module.exports = {
     'arrow-spacing': [2, {before: true, after: true}],
     'rest-spread-spacing': [2, 'never'],
     'template-curly-spacing': [2, 'never'],
-    'no-return-await': [2],
     'require-await': [2],
     'no-await-in-loop': [2],
     'prefer-promise-reject-errors': [2],
